29d4e1dcbb1e25634dba4b0bf7718b657f77edbb
Alexander Andrenko <andrenkom@gmail.com>
2021-06-06 23:46:08 +0700

    feat: Begin implement Entities classes

---
+++ Kindergarten/Kindergarten/Models/Entities/Dish.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kindergarten.Models.Entities
+{
+    class Dish
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int DishNurseryNorm { get; set; }
+        public int DishYardNorm { get; set; }
+
+        public ICollection<DishItem> DishItems { get; set; }
+
+        //Навигационные свойства
+        public int MenuId { get; set; }
+        public Menu Menu { get; set; }
+    }
+}
---
+++ Kindergarten/Kindergarten/Models/Entities/DishItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kindergarten.Models.Entities
+{
+    class DishItem
+    {
+        public int Id { get; set; }
+        public  int NurseryNorm { get; set; }
+        public int YardNorm { get; set; }
+
+        //Навигационные свойства
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+
+        public int DishId { get; set; }
+        public Dish Dish { get; set; }
+    }
+}
